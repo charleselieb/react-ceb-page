55fa580f35577afe5a7b8b049b76fadbfa8993b1
Charles-Elie Bedard <charleselie@gmail.com>
2020-03-31 19:04:08 -0400

    Updates

---
+++ precache-manifest.c6a077f85562162d0ebc1f1af6a69da3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a8fc8d7ad5deb49cc51585ea605746",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f27e0d223c9bb8e5c24",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a1a1946f8e21155a0f0b",
+    "url": "/static/js/2.f63ce48f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f63ce48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f27e0d223c9bb8e5c24",
+    "url": "/static/js/main.c7967870.chunk.js"
+  },
+  {
+    "revision": "7634b1e5d127024ad9cb",
+    "url": "/static/js/runtime-main.de57d695.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
