b6e2f9c120b617b6097699b24288e90c140c42de
Charles-Elie Bedard <charleselie@gmail.com>
2020-03-31 20:39:32 -0400

    add emotion library and change landing title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
+// this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
 import logo from './logo.svg';
 import './App.css';
+
+const style = {
+  title: css`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap');
+    font-family: 'Roboto', sans-serif;
+    font-weight: 500;
+  `
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 css={style.title}>Hello World!</h1>
       </header>
     </div>
   );
